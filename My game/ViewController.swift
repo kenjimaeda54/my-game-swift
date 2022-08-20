30211b6cbd72b315374b9e2bcb9ee9a4cbf9e900
kenjimaeda54 <kenjimaedafamily@gmail.com>
2022-08-20 12:48:15 -0300

    Initial Commit

---
+++ My game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My game
+//
+//  Created by kenjimaeda on 20/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
